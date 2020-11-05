35e944d30e0918b2b4df1bc3b76fa3a3ed31c7d3
Augusto <aht@cin.ufpe.br>
2020-11-05 12:38:06 -0300

    Challenge 05 resolution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    let income = 0;
+    let outcome = 0;
+
+    for (let i = 0; i < this.transactions.length; i++) {
+      if(this.transactions[i].type === 'income') income += this.transactions[i].value;
+      else if(this.transactions[i].type === 'outcome') outcome += this.transactions[i].value;
+    }
+
+    const balance = {
+      income: income,
+      outcome: outcome,
+      total: income - outcome
+    }
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO ): Transaction {
+
+    if(type === 'outcome' && this.getBalance().total < value){
+      console.log('Erro')
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
